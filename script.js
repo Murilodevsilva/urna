b4e261e773792d38d952e37bccf8be8dfdeb61dd
Murilo da Silva Riba <106834206+Murilodevsilva@users.noreply.github.com>
2022-06-28 11:56:42 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,83 @@
+//começo da interface
+let yourVoteFor = document.querySelector ('.d-1-1 span');
+let office = document.querySelector('.d-1-2 span');
+let description = document.querySelector('.d-1-4');
+let warning = document.querySelector('.d-2');
+let image = document.querySelector('.d-1-right');
+let numbers = document.querySelector('.d-1-3');
+//fim da interface
+let stapCurrent = 0;
+let number ='';
+
+function startStage() {
+    let stap = staps [stapCurrent];
+
+    let numberHtml = '';
+
+    for(let i=0;i<stap.numbers;i++){
+        if(i === 0){
+            numberHtml += '<div class="numero pisca"></div>'
+        } else{
+            numberHtml += '<div class="numero"></div>';
+        }
+    }
+    yourVoteFor.style.display = 'none';
+    office.innerHTML = stap.tittle;
+    description.innerHTML = '';
+    warning.style.display = 'none';
+    image.innerHTML = '';
+    numbers.innerHTML = numberHtml;
+}
+function updateInterface (){
+     let stap = staps[startStage];
+     let candidates = stap.candidates.filter((item)=>{
+        if(item.number === number){
+            return true;
+        }else{
+            return false;
+        }
+     });
+
+     if(candidates.length > 0){
+        candidates = candidates[0];
+        yourVoteFor.style.display = 'block';
+        description.innerHTML = `Nome: ${candidates.name}<br/>Partido: ${candidates.part}`;
+        warning.style.display ='block'
+
+        let photoHtml = '';
+          for(let i in candidate.photo){
+            photoHtml += `<div class="d-1-image"><img src="img/${candidates.photo[i].url}" alt="">${candidates.photo[i].legend}</div>`
+          }
+        image.innerHTML = photoHtml;
+     }
+
+}
+
+function clicks(n) {
+    let numberEl = document.querySelector(".numero.pisca");
+    if(numberEl !== null){
+        numberEl.innerHTML = n;
+        number = `${number}${n}`;
+
+        numberEl.classList.remove('pisca');
+        if(numberEl.nextElementSibling){
+            numberEl.nextElementSibling.classList.add('pisca');
+        }else {
+            updateInterface();
+        }
+    }else {
+        updateInterface();
+    }
+}
+function white(){
+    alert("Clicou em BRANCO!");
+}
+function correct(){
+    alert("Clicou em CORRIGE!");
+}
+function comfirm(){
+    alert("Clicou em COMFIRMA!");
+}
+
+
+startStage();
---
+++ staps.js
@@ -0,0 +1,50 @@
+let staps = [
+    {
+        tittle: 'VEREADOR',
+        numbers: 5,
+        candidates:[
+            {
+                number:'38111',
+                name: 'Danilo',
+                part: 'PSB',
+                photo:[
+                    {url:'ve1.png', legend:'Vereador'}
+                ]
+            },
+            {
+                number:'88568',
+                name: 'Nadi',
+                part: 'PR',
+                photo: [
+                    {url:'ve5.png', legend:'Vereador'}
+                ]
+            },
+        ]
+    },
+    {
+        tittle:'PRESIDENTE',
+        numbers:2,
+        candidates:[
+            {
+                number:'22',
+                name:'Jair Messias Bolsonaro',
+                part:'PL',
+                vice: 'Danilo',
+                photo:[
+                    {url:'camada 1.png',legend:'Presidente'},
+                    {url:'ve1.png', legend:'Vice', small:true}
+                ]
+            },
+            {
+                number:'22',
+                name:'Luiz Inácio Lula da Silva',
+                part:'PT',
+                vice: 'Damasceno',
+                photo:[
+                    {url:'camada 2.png',legend:'Presidente'},
+                    {url:'ve2.png', legend:'Vice', small:true}
+                ]
+            }
+        ]
+    }
+]
